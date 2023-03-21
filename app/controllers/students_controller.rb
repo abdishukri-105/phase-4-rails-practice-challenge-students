599a5286e7e5551e558687cf18155646be504909
abdishukri <abdishukrimohamed105@gmail.com>
2023-03-21 12:15:26 +0300

    added students routes and controller actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,52 @@
 class StudentsController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
+      def index
+          students = Student.all
+          render json: students
+      end
+  
+      def show
+        student = find_student
+        render json: student
+      end
+      
+      def destroy
+        student = find_student
+        student.destroy
+        head :no_content
+      end
+  
+      def create
+        student = Student.create!(student_params)
+        render json: student.to_json(except: [:created_at, :updated_at]), status: :created
+      end
+
+      def update
+        student = find_student
+        student = Student.update(student_params)
+        render json: student.to_json(except: [:created_at, :updated_at]), status: :created
+      end
+
+  private
+
+    def find_student
+      Student.find(params[:id])
+    end
+  
+    def student_params
+      params.permit(:name, :age, :instructor_id)
+    end
+  
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+    def render_not_found_response
+       render json: { error: "student not found" }, status: :not_found
+    end
+
 end
